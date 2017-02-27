95c91e90ba73ed1f726d77935638c633099ad58b
Alex Regan <alex.regan@artisancolour.com>
2017-02-27 15:17:14 -0700

    createShortcut now accepts an array of sequences

--- dist/bundle.js
+++ dist/bundle.js
@@ -5,6 +5,8 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
@@ -16,9 +18,6 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 
 	var keycode = require('@alexsasharegan/keycodes');
 	var Emitter = require('component-emitter');
-
-	var keydown = '__keydown';
-	var keyup = '__keyup';
 
 	var Shortcuts = function (_Emitter) {
 		_inherits(Shortcuts, _Emitter);
@@ -33,15 +32,25 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 			_this.handleKeyDown = _this.handleKeyDown.bind(_this);
 			_this.handleKeyUp = _this.handleKeyUp.bind(_this);
 
+			_this._isListening = false;
+			_this._modKeys = [91, 93, 224];
 			_this._keyMap = {};
 			_this._shortcuts = {};
+			_this._internalMap = {
+				keydown: '__keydown',
+				keyup: '__keyup'
+			};
+			_this._eventMap = {
+				node: node,
+				events: {
+					keydown: _this.handleKeyDown,
+					keyup: _this.handleKeyUp
+				}
+			};
 
-			_this.on(keydown, _this.handleShortcuts);
-
-			Shortcuts.addEvents(node, {
-				keydown: _this.handleKeyDown,
-				keyup: _this.handleKeyUp
-			});
+			_this.on(_this._internalMap.keydown, _this.handleShortcuts);
+			Shortcuts.addEvents(node, _this._eventMap.events);
+			_this._isListening = true;
 			return _this;
 		}
 
@@ -52,7 +61,11 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 				var code = keycode(event, true);
 
 				this._keyMap[code] = keycode(code);
-				this.emit(keydown, event);
+				this.emit(this._internalMap.keydown, event);
+
+				if (event.metaKey && !~this._modKeys.indexOf(code)) {
+					this.handleKeyUp(event);
+				}
 			}
 		}, {
 			key: 'handleKeyUp',
@@ -61,7 +74,7 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 				var code = keycode(event, true);
 
 				delete this._keyMap[code];
-				this.emit(keyup, event);
+				this.emit(this._internalMap.keyup, event);
 			}
 		}, {
 			key: 'handleShortcuts',
@@ -69,45 +82,101 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 				var _this2 = this;
 
 				Object.keys(this._shortcuts).forEach(function (shortcut) {
-					var sequence = _this2._shortcuts[shortcut];
-					var shouldFire = true;
+					// at a named shortcut, we have an array of possible key sequences
+					// inside each sequence is an array of keycodes
+					// each keycode can itself be an array because the same key can appear at multiple codes
+					var sequences = _this2._shortcuts[shortcut];
+					var shouldEmit = false;
 
-					for (var i = 0; i < sequence.length; i++) {
-						if (!_this2.testKeyDown(sequence[i])) {
-							shouldFire = false;
+					// loop through each shortcut sequence
+					for (var i = 0; i < sequences.length; i++) {
+						var sequence = sequences[i];
+						var thisSequenceTriggers = true;
+
+						// loop through the keycodes in the sequence
+						// and test that each key is pressed
+						for (var j = 0; j < sequence.length; j++) {
+							// fail fast if a key is not detected
+							if (!_this2.testKeyCode(sequence[j])) {
+								thisSequenceTriggers = false;
+								break;
+							}
+						}
+
+						// quit looping as soon as a sequence is satisfied
+						if (thisSequenceTriggers) {
+							shouldEmit = true;
 							break;
 						}
 					}
 
 					// TODO: dev logging
-					console.log({ shouldFire: shouldFire, keymap: _this2._keyMap });
+					console.log({ shouldEmit: shouldEmit, keymap: _this2._keyMap, listeners: _this2.listeners(shortcut) });
 
-					if (shouldFire) _this2.emit(shortcut, event);
+					if (shouldEmit) _this2.emit(shortcut, event);
 				});
 			}
 		}, {
 			key: 'createShortcut',
 			value: function createShortcut(name, keys) {
+				var _this3 = this;
+
+				if (Array.isArray(keys)) {
+					keys.forEach(function (key) {
+						return _this3.createShortcut(name, key);
+					});
+					return this;
+				}
 				// normalize input & transform to keycodes
-				this._shortcuts[name] = (Array.isArray(keys) ? keys : keys.split('+')).map(function (key) {
-					return keycode(key, true);
-				});
+				var shortcuts = this._shortcuts[name] || [];
+				this._shortcuts[name] = [].concat(_toConsumableArray(shortcuts), [keys.split('+').map(function (key) {
+					return keycode(key.trim(), true);
+				})]);
 
 				return this;
 			}
 		}, {
-			key: 'testKeyDown',
-			value: function testKeyDown(code) {
+			key: 'testKeyCode',
+			value: function testKeyCode(code) {
 				if (Array.isArray(code)) {
+					// a code can be an array when there are multiple codes possible to trigger the same key
+					// should return true upon the first match in the active key map
 					for (var i = 0; i < code.length; i++) {
-						var valid = !!this._keyMap[code[i]];
-						if (valid) return valid;
-					}
-
+						if (!!this._keyMap[code[i]]) return true;
+					} // all tested values not found on the map, return false
 					return false;
 				} else {
 					return !!this._keyMap[code];
 				}
+			}
+		}, {
+			key: 'pauseListening',
+			value: function pauseListening() {
+				if (this._isListening) {
+					Shortcuts.removeEvents(this._eventMap.node, this._eventMap.events);
+					this._isListening = false;
+				}
+
+				return this;
+			}
+		}, {
+			key: 'resumeListening',
+			value: function resumeListening() {
+				if (!this._isListening) {
+					Shortcuts.addEvents(this._eventMap.node, this._eventMap.events);
+					this._isListening = true;
+				}
+
+				return this;
+			}
+		}, {
+			key: 'destroy',
+			value: function destroy() {
+				this.off();
+				Shortcuts.removeEvents(this._eventMap.node, this._eventMap.events);
+				this._isListening = false;
+
+				return this;
 			}
 		}], [{
 			key: 'addEvent',
@@ -140,15 +209,25 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 				}
 			}
 		}, {
-			key: 'addEventOnce',
-			value: function addEventOnce(node, event, callback) {
-				function fnOnce() {
-					Shortcuts.removeEvent(node, event, fnOnce);
+			key: 'removeEvents',
+			value: function removeEvents(node) {
+				var eventMap = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
 
-					return callback.apply(this, arguments);
+				var _loop2 = function _loop2(event) {
+					var handlers = eventMap[event];
+
+					if (typeof handlers === 'function') {
+						Shortcuts.removeEvent(node, event, handlers);
+					} else {
+						handlers.forEach(function (handler) {
+							return Shortcuts.removeEvent(node, event, handler);
+						});
+					}
+				};
+
+				for (var event in eventMap) {
+					_loop2(event);
 				}
-
-				Shortcuts.addEvent(node, event, fnOnce);
 			}
 		}, {
 			key: 'removeEvent',
@@ -159,11 +238,6 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 
 		return Shortcuts;
 	}(Emitter);
-
-	// TODO: for testing, expose globally
-
-
-	window.Shortcuts = Shortcuts;
 
 	if (exports) module.exports = Shortcuts;else if (global) global.Shortcuts = Shortcuts;
 
--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@
 	const keycode = require( '@alexsasharegan/keycodes' );
 	const Emitter = require( 'component-emitter' );
 	
-	const keydown = '__keydown';
-	const keyup   = '__keyup';
-	
 	class Shortcuts extends Emitter {
 		
 		constructor( node = window ) {
@@ -15,15 +12,25 @@
 			this.handleKeyDown = this.handleKeyDown.bind( this );
 			this.handleKeyUp   = this.handleKeyUp.bind( this );
 			
-			this._keyMap    = {};
-			this._shortcuts = {};
+			this._isListening = false;
+			this._modKeys     = [ 91, 93, 224 ];
+			this._keyMap      = {};
+			this._shortcuts   = {};
+			this._internalMap = {
+				keydown: '__keydown',
+				keyup: '__keyup',
+			};
+			this._eventMap    = {
+				node,
+				events: {
+					keydown: this.handleKeyDown,
+					keyup: this.handleKeyUp,
+				}
+			};
 			
-			this.on( keydown, this.handleShortcuts );
-			
-			Shortcuts.addEvents( node, {
-				keydown: this.handleKeyDown,
-				keyup: this.handleKeyUp,
-			} );
+			this.on( this._internalMap.keydown, this.handleShortcuts );
+			Shortcuts.addEvents( node, this._eventMap.events );
+			this._isListening = true;
 		}
 		
 		handleKeyDown( event ) {
@@ -31,7 +38,11 @@
 			const code = keycode( event, true );
 			
 			this._keyMap[ code ] = keycode( code );
-			this.emit( keydown, event );
+			this.emit( this._internalMap.keydown, event );
+			
+			if ( event.metaKey && !~this._modKeys.indexOf( code ) ) {
+				this.handleKeyUp( event );
+			}
 		}
 		
 		handleKeyUp( event ) {
@@ -39,49 +50,99 @@
 			const code = keycode( event, true );
 			
 			delete this._keyMap[ code ];
-			this.emit( keyup, event );
+			this.emit( this._internalMap.keyup, event );
 		}
 		
 		handleShortcuts( event ) {
 			Object.keys( this._shortcuts )
 			      .forEach( shortcut => {
-				      const sequence = this._shortcuts[ shortcut ];
-				      let shouldFire = true;
+				      // at a named shortcut, we have an array of possible key sequences
+				      // inside each sequence is an array of keycodes
+				      // each keycode can itself be an array because the same key can appear at multiple codes
+				      const sequences = this._shortcuts[ shortcut ];
+				      let shouldEmit  = false;
 				
-				      for ( let i = 0; i < sequence.length; i++ ) {
-					      if ( !this.testKeyDown( sequence[ i ] ) ) {
-						      shouldFire = false;
+				      // loop through each shortcut sequence
+				      for ( let i = 0; i < sequences.length; i++ ) {
+					      let sequence             = sequences[ i ];
+					      let thisSequenceTriggers = true;
+					
+					      // loop through the keycodes in the sequence
+					      // and test that each key is pressed
+					      for ( let j = 0; j < sequence.length; j++ ) {
+						      // fail fast if a key is not detected
+						      if ( !this.testKeyCode( sequence[ j ] ) ) {
+							      thisSequenceTriggers = false;
+							      break;
+						      }
+					      }
+					
+					      // quit looping as soon as a sequence is satisfied
+					      if ( thisSequenceTriggers ) {
+						      shouldEmit = true;
 						      break;
 					      }
 				      }
 				
 				      // TODO: dev logging
-				      console.log( { shouldFire, keymap: this._keyMap } );
+				      console.log( { shouldEmit, keymap: this._keyMap, listeners: this.listeners( shortcut ), } );
 				
-				      if ( shouldFire ) this.emit( shortcut, event );
+				      if ( shouldEmit ) this.emit( shortcut, event );
 			      } );
 		}
 		
 		createShortcut( name, keys ) {
+			if ( Array.isArray( keys ) ) {
+				keys.forEach( key => this.createShortcut( name, key ) );
+				return this;
+			}
 			// normalize input & transform to keycodes
-			this._shortcuts[ name ] = (
-				Array.isArray( keys ) ? keys : keys.split( '+' )
-			).map( key => keycode( key, true ) );
+			const shortcuts         = this._shortcuts[ name ] || [];
+			this._shortcuts[ name ] = [
+				...shortcuts,
+				keys.split( '+' ).map( key => keycode( key.trim(), true ) )
+			];
 			
 			return this;
 		}
 		
-		testKeyDown( code ) {
+		testKeyCode( code ) {
 			if ( Array.isArray( code ) ) {
-				for ( let i = 0; i < code.length; i++ ) {
-					let valid = !!this._keyMap[ code[ i ] ];
-					if ( valid ) return valid;
-				}
+				// a code can be an array when there are multiple codes possible to trigger the same key
+				// should return true upon the first match in the active key map
+				for ( let i = 0; i < code.length; i++ ) if ( !!this._keyMap[ code[ i ] ] ) return true;
 				
+				// all tested values not found on the map, return false
 				return false;
 			} else {
 				return !!this._keyMap[ code ];
 			}
+		}
+		
+		pauseListening() {
+			if ( this._isListening ) {
+				Shortcuts.removeEvents( this._eventMap.node, this._eventMap.events );
+				this._isListening = false;
+			}
+			
+			return this;
+		}
+		
+		resumeListening() {
+			if ( !this._isListening ) {
+				Shortcuts.addEvents( this._eventMap.node, this._eventMap.events );
+				this._isListening = true;
+			}
+			
+			return this;
+		}
+		
+		destroy() {
+			this.off();
+			Shortcuts.removeEvents( this._eventMap.node, this._eventMap.events );
+			this._isListening = false;
+			
+			return this;
 		}
 		
 		static addEvent( node, event, callback ) {
@@ -104,23 +165,22 @@
 			}
 		}
 		
-		static addEventOnce( node, event, callback ) {
-			function fnOnce() {
-				Shortcuts.removeEvent( node, event, fnOnce );
+		static removeEvents( node, eventMap = {} ) {
+			for ( let event in eventMap ) {
+				const handlers = eventMap[ event ];
 				
-				return callback.apply( this, arguments );
+				if ( typeof handlers === 'function' ) {
+					Shortcuts.removeEvent( node, event, handlers );
+				} else {
+					handlers.forEach( handler => Shortcuts.removeEvent( node, event, handler ) );
+				}
 			}
-			
-			Shortcuts.addEvent( node, event, fnOnce );
 		}
 		
 		static removeEvent( node, event, callback ) {
 			node.removeEventListener( event, callback, false );
 		}
 	}
-	
-	// TODO: for testing, expose globally
-	window.Shortcuts = Shortcuts;
 	
 	if ( exports ) module.exports = Shortcuts;
 	else if ( global ) global.Shortcuts = Shortcuts;
